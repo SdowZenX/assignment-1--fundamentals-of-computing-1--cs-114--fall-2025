1242c4e21c77a707a73289e900e4e61eaffb80d3
SdowZenX <zanes11zs@gmail.com>
2025-09-18 14:50:53 -0400

    Update Assignment1Program1.java

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -1,5 +1,28 @@
+import java.util.Random;
+import java.util.Scanner;
 public class Assignment1Program1 {
   public static void main(String[] args) {
+System.out.println("ZZZZZZ  SSSSS  ");
+System.out.println("    Z   S       ");
+System.out.println("   Z    S       ");
+System.out.println("  Z      SSS    ");
+System.out.println(" Z          S   ");
+System.out.println("Z           S   ");
+System.out.println("ZZZZZZ  SSSSS");}
+int temp, num1;
+double converted;
+String line, newline;
 
-  }
-}
+//Get user input
+Scanner scan = new Scanner(System.in);
+
+
+System.out.@println("Please enter a 5 character string:")
+//Trim the first and last characters
+String trimmedString = userstring.substring(1,4);
+//Reverse the trimmed string
+String reversedString = StringBuilder(trimmedString).reverse().tostring();}
+System.out.@println(sb);
+
+System.out.Println("Please enter temp in fahrenheit:");
+temp = scan.nextInt();
